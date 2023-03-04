40f552b94ad45be61b74558c3ab8a3304409e3d0
Nick Moreton <nickmoreton@me.com>
2023-03-04 15:37:30 +0000

    silence date warning

--- testapp/settings/base.py
+++ testapp/settings/base.py
@@ -124,15 +124,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+# TIME_ZONE = "UTC"
 
-USE_I18N = True
+# USE_I18N = True
 
-USE_L10N = True
+# USE_L10N = True
 
-USE_TZ = True
+# USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
