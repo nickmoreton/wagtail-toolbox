5292aac3de689b26f32d19c6073bc62d0a44b7ce
Nick Moreton <nick.moreton@torchbox.com>
2023-03-17 22:10:23 +0000

    Tidy up wordpress.py

--- wagtail_toolbox/wordpress/models/wordpress.py
+++ wagtail_toolbox/wordpress/models/wordpress.py
@@ -1,14 +1,18 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel, FieldRowPanel
 
 
 class WordpressModel(models.Model):
-    """ABSTRACT Base model for Wordpress models that will be imported."""
+    """ABSTRACT Base model for the Wordpress models.
 
-    SOURCE_URL = None  # e.g. /wp-json/wp/v2/[model_name]
+    All Wordpress models should inherit from this model.
+    """
+
+    # Format: /wp-json/wp/v2/[model_name]
+    # This is passed to the wordpress client and appended to WP_HOST
+    # It's required on every wordpress model
+    SOURCE_URL = None
 
     def __init__(self, *args, **kwargs):
-        """Set the source URL."""
         super().__init__(*args, **kwargs)
         if not self.SOURCE_URL:
             raise NotImplementedError("WordpressModel must have a SOURCE_URL attribute")
@@ -23,30 +27,9 @@ class WordpressModel(models.Model):
 
     @property
     def get_title(self):
-        """Get the title for the Wordpress object either name or title depending on the model."""
+        """Get the title for the Wordpress object
+        either name or title depending on the model."""
         return self.title if hasattr(self, "title") else self.name
-
-    @staticmethod
-    def process_foreign_keys():
-        """Override this method to process foreign keys.
-        When importing from the Wordpress JSON API"""
-        return []
-
-    @staticmethod
-    def process_many_to_many_keys():
-        """Override this method to process many to many keys.
-        When importing from the Wordpress JSON API"""
-        return []
-
-    @staticmethod
-    def process_fields():
-        """Override this method to process fields.
-        When importing from the Wordpress JSON API"""
-        return []
-
-    def get_source_url(self):
-        """Get the source URL for the Wordpress object."""
-        return self.SOURCE_URL.strip("/")
 
     def exclude_fields_initial_import(self):
         """Fields to exclude from the initial import."""
@@ -62,6 +45,25 @@ class WordpressModel(models.Model):
 
         return exclude_foreign_keys + exclude_many_to_many_keys
 
+    @staticmethod
+    def process_fields():
+        """Override this method to process fields."""
+        return []
+
+    @staticmethod
+    def process_foreign_keys():
+        """Override this method to process foreign keys."""
+        return []
+
+    @staticmethod
+    def process_many_to_many_keys():
+        """Override this method to process many to many keys."""
+        return []
+
+    def get_source_url(self):
+        """Get the source URL for the Wordpress object."""
+        return self.SOURCE_URL.strip("/")
+
 
 class WPCategory(WordpressModel):
     """Model definition for Category."""
@@ -73,30 +75,13 @@ class WPCategory(WordpressModel):
     link = models.URLField()
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
+    taxonomy = models.CharField(max_length=255)
     parent = models.ForeignKey(
         "wordpress.WPCategory",
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
     )
-    taxonomy = models.CharField(max_length=255)
-
-    panels = [
-        FieldPanel("name"),
-        FieldPanel("link"),
-        FieldPanel("slug"),
-        FieldPanel("description"),
-        FieldRowPanel(
-            [
-                FieldPanel("parent"),
-                FieldPanel("taxonomy"),
-                FieldPanel("count"),
-            ]
-        ),
-        FieldPanel("wp_id"),
-        FieldPanel("wp_foreign_keys"),
-        FieldPanel("wp_many_to_many_keys"),
-    ]
 
     class Meta:
         verbose_name = "Category"
@@ -135,23 +120,6 @@ class WPTag(WordpressModel):
     def __str__(self):
         return self.name
 
-    panels = [
-        FieldPanel("name"),
-        FieldPanel("link"),
-        FieldPanel("slug"),
-        FieldPanel("description"),
-        FieldRowPanel(
-            [
-                # FieldPanel("parent"),
-                FieldPanel("taxonomy"),
-                FieldPanel("count"),
-            ]
-        ),
-        FieldPanel("wp_id"),
-        FieldPanel("wp_foreign_keys"),
-        FieldPanel("wp_many_to_many_keys"),
-    ]
-
 
 class WPAuthor(WordpressModel):
     """Model definition for Author. AKA User."""
@@ -172,18 +140,6 @@ class WPAuthor(WordpressModel):
     def __str__(self):
         return self.name
 
-    panels = [
-        FieldPanel("name"),
-        FieldPanel("url"),
-        FieldPanel("link"),
-        FieldPanel("slug"),
-        FieldPanel("description"),
-        FieldPanel("avatar_urls"),
-        FieldPanel("wp_id"),
-        FieldPanel("wp_foreign_keys"),
-        FieldPanel("wp_many_to_many_keys"),
-    ]
-
 
 class WPPost(WordpressModel):
     """Model definition for Post."""
@@ -202,18 +158,25 @@ class WPPost(WordpressModel):
     link = models.URLField()
     content = models.TextField(blank=True, null=True)
     excerpt = models.TextField(blank=True, null=True)
-    author = models.ForeignKey(
-        WPAuthor, on_delete=models.SET_NULL, blank=True, null=True
-    )
-    categories = models.ManyToManyField(WPCategory, blank=True)
-    tags = models.ManyToManyField(WPTag, blank=True)
-
-    # featured_media = models.IntegerField(default=0)
     comment_status = models.CharField(max_length=255)
     ping_status = models.CharField(max_length=255)
     sticky = models.BooleanField(default=False)
     format = models.CharField(max_length=255)
     template = models.CharField(max_length=255, null=True, blank=True)
+    author = models.ForeignKey(
+        WPAuthor,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    categories = models.ManyToManyField(
+        WPCategory,
+        blank=True,
+    )
+    tags = models.ManyToManyField(
+        WPTag,
+        blank=True,
+    )
 
     class Meta:
         verbose_name = "Post"
@@ -222,60 +185,10 @@ class WPPost(WordpressModel):
     def __str__(self):
         return self.title
 
-    panels = [
-        FieldPanel("title"),
-        FieldRowPanel(
-            [
-                FieldPanel("date"),
-                FieldPanel("date_gmt"),
-            ]
-        ),
-        FieldPanel("guid"),
-        FieldRowPanel(
-            [
-                FieldPanel("modified"),
-                FieldPanel("modified_gmt"),
-            ]
-        ),
-        FieldPanel("slug"),
-        FieldRowPanel(
-            [
-                FieldPanel("status"),
-                FieldPanel("type"),
-                FieldPanel("link"),
-            ]
-        ),
-        FieldPanel("content"),
-        FieldPanel("excerpt"),
-        FieldRowPanel(
-            [
-                FieldPanel("author"),
-                FieldPanel("categories"),
-                FieldPanel("tags"),
-            ]
-        ),
-        FieldRowPanel(
-            [
-                FieldPanel("comment_status"),
-                FieldPanel("ping_status"),
-                FieldPanel("sticky"),
-                FieldPanel("format"),
-                FieldPanel("template"),
-            ]
-        ),
-        FieldPanel("wp_id"),
-        FieldPanel("wp_foreign_keys"),
-        FieldPanel("wp_many_to_many_keys"),
-    ]
-
     @staticmethod
     def process_foreign_keys():
         """These are excluded from the first import and processed later."""
-        return [
-            {
-                "author": {"model": "WPAuthor", "field": "wp_id"},
-            }
-        ]
+        return [{"author": {"model": "WPAuthor", "field": "wp_id"}}]
 
     @staticmethod
     def process_many_to_many_keys():
@@ -315,21 +228,22 @@ class WPPage(WordpressModel):
     link = models.URLField()
     content = models.TextField(blank=True, null=True)
     excerpt = models.TextField(blank=True, null=True)
-    author = models.ForeignKey(
-        WPAuthor, on_delete=models.SET_NULL, blank=True, null=True
-    )
-    # categories = models.ManyToManyField(WPCategory, blank=True)
-    # tags = models.ManyToManyField(WPTag, blank=True)
-
-    # featured_media = models.IntegerField(default=0)
-    parent = models.ForeignKey(
-        "wordpress.WPPage", on_delete=models.SET_NULL, blank=True, null=True
-    )
     comment_status = models.CharField(max_length=255)
     ping_status = models.CharField(max_length=255)
     sticky = models.BooleanField(default=False)
-    # format = models.CharField(max_length=255)
     template = models.CharField(max_length=255, null=True, blank=True)
+    author = models.ForeignKey(
+        "wordpress.WPAuthor",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
+    parent = models.ForeignKey(
+        "wordpress.WPPage",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         verbose_name = "Page"
@@ -337,36 +251,6 @@ class WPPage(WordpressModel):
 
     def __str__(self):
         return self.title
-
-    panels = [
-        FieldPanel("title"),
-        FieldRowPanel([FieldPanel("date"), FieldPanel("date_gmt")]),
-        FieldPanel("guid"),
-        FieldRowPanel([FieldPanel("modified"), FieldPanel("modified_gmt")]),
-        FieldPanel("slug"),
-        FieldRowPanel(
-            [
-                FieldPanel("status"),
-                FieldPanel("type"),
-                FieldPanel("link"),
-                FieldPanel("parent"),
-            ]
-        ),
-        FieldPanel("content"),
-        FieldPanel("excerpt"),
-        FieldRowPanel(
-            [
-                FieldPanel("author"),
-                FieldPanel("comment_status"),
-                FieldPanel("ping_status"),
-                FieldPanel("sticky"),
-                FieldPanel("template"),
-            ]
-        ),
-        FieldPanel("wp_id"),
-        FieldPanel("wp_foreign_keys"),
-        FieldPanel("wp_many_to_many_keys"),
-    ]
 
     @staticmethod
     def process_foreign_keys():
@@ -394,25 +278,34 @@ class WPComment(WordpressModel):
 
     SOURCE_URL = "/wp-json/wp/v2/comments"
 
-    post = models.ForeignKey(WPPost, on_delete=models.CASCADE, blank=True, null=True)
-    parent = models.ForeignKey(
-        "wordpress.WPComment", on_delete=models.CASCADE, blank=True, null=True
-    )
-    # not sure if this is needed as they all seem to be zero
-    author = models.ForeignKey(
-        WPAuthor, on_delete=models.SET_NULL, blank=True, null=True
-    )
     author_name = models.CharField(max_length=255)
     author_url = models.URLField()
     date = models.DateTimeField()
     date_gmt = models.DateTimeField()
     content = models.TextField(blank=True, null=True)
     link = models.URLField()
-    status = models.CharField(
-        max_length=255, null=True, blank=True
-    )  # TODO: check if this should be boolean
+    status = models.CharField(max_length=255, null=True, blank=True)
     type = models.CharField(max_length=255)
     author_avatar_urls = models.URLField(blank=True, null=True)
+    post = models.ForeignKey(
+        "wordpress.WPPost",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    parent = models.ForeignKey(
+        "wordpress.WPComment",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    # is this needed as they all seem to be zero anyway?
+    author = models.ForeignKey(
+        "wordpress.WPAuthor",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
     class Meta:
         verbose_name = "Comment"
@@ -420,20 +313,6 @@ class WPComment(WordpressModel):
 
     def __str__(self):
         return self.author_name
-
-    panels = [
-        FieldPanel("post"),
-        FieldPanel("parent"),
-        FieldPanel("author"),
-        FieldPanel("author_name"),
-        FieldPanel("author_url"),
-        FieldRowPanel([FieldPanel("date"), FieldPanel("date_gmt")]),
-        FieldPanel("content"),
-        FieldPanel("link"),
-        FieldPanel("status"),
-        FieldPanel("type"),
-        FieldPanel("author_avatar_urls"),
-    ]
 
     @staticmethod
     def process_fields():
@@ -460,9 +339,6 @@ class WPMedia(WordpressModel):
     SOURCE_URL = "/wp-json/wp/v2/media"
 
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(
-        "wordpress.WPAuthor", on_delete=models.SET_NULL, blank=True, null=True
-    )
     date = models.DateTimeField()
     date_gmt = models.DateTimeField()
     guid = models.URLField()
@@ -481,8 +357,17 @@ class WPMedia(WordpressModel):
     media_type = models.CharField(max_length=255)
     mime_type = models.CharField(max_length=255)
     source_url = models.URLField()
+    author = models.ForeignKey(
+        "wordpress.WPAuthor",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
     post = models.ForeignKey(
-        "wordpress.WPPost", on_delete=models.CASCADE, blank=True, null=True
+        "wordpress.WPPost",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
     )
 
     class Meta:
@@ -511,63 +396,3 @@ class WPMedia(WordpressModel):
                 "post": {"model": "WPPost", "field": "wp_id"},
             }
         ]
-
-    panels = [
-        FieldPanel("title"),
-        FieldRowPanel([FieldPanel("date"), FieldPanel("date_gmt")]),
-        FieldPanel("guid"),
-        FieldRowPanel([FieldPanel("modified"), FieldPanel("modified_gmt")]),
-        FieldPanel("slug"),
-        FieldRowPanel(
-            [
-                FieldPanel("author"),
-                FieldPanel("status"),
-                FieldPanel("type"),
-                FieldPanel("link"),
-            ]
-        ),
-        FieldPanel("description"),
-        FieldPanel("caption"),
-        FieldPanel("alt_text"),
-        FieldPanel("media_type"),
-        FieldPanel("mime_type"),
-        FieldPanel("source_url"),
-        FieldPanel("post"),
-    ]
-
-
-# def get_target_mapping(source):
-#     if hasattr(settings, "WPI_TARGET_MAPPING"):
-#         source = source.split("/")[-1]
-#         model_mapping = settings.WPI_TARGET_MAPPING.get(source, None)
-#         return model_mapping
-
-
-# def get_model_type(config):
-#     """Deal with save actions differently for wagtail pages vs django models"""
-
-#     model_type = (
-#         "page"
-#         if "model_type" in config.keys() and config["model_type"] == "page"
-#         else "model"
-#     )
-
-#     return model_type
-
-
-# def get_many_to_many_mapping(config):
-#     """Get the many to many mapping from the config"""
-#     return config.get("many_to_many_mapping", None)
-
-
-# def get_related_mapping(config):
-#     """Get the related mapping from the config"""
-#     return config.get("related_mapping", None)
-
-
-# def get_target_model(config):
-#     """Get the target model from the config"""
-#     return apps.get_model(
-#         app_label=config["target_model"][0],
-#         model_name=config["target_model"][1],
-#     )
